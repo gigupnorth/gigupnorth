2c871b0cc451926169b137b56f7494c775aaad1a
gigupnorth <gigupnorth@gmail.com>
2026-02-06 17:19:51 +0000

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ const venueImages = {
   "NE Volume Bar": "https://gigupnorth.github.io/gigupnorth/images/nevolume.jpg",
   "Cobalt Studios": "https://gigupnorth.github.io/gigupnorth/images/cobalt.jpg",
   "The Cluny": "https://gigupnorth.github.io/gigupnorth/images/cluny1.jpg",
-  "Little Buildings": "https://gigupnorth.github.io/gigupnorth/images/littlb.jpg"
+  "Little Buildings": "https://gigupnorth.github.io/gigupnorth/images/littlb.jpg",
+   "The Forum": "https://gigupnorth.github.io/gigupnorth/images/forum.jpg"
 };
 
 
